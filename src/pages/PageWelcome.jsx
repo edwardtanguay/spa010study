83bb72aa16d47decdd1c9865d1466a7af2268199
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-19 12:00:04 +0200

    toggle button on welcome page works

--- src/pages/PageWelcome.jsx
+++ src/pages/PageWelcome.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 export const PageWelcome = () => {
+	const [status, setStatus] = useState('offline');
+
 	return (
 		<div className="page_welcome">
 			<h2>Welcome</h2>
 			<p>Welcome to this site.</p>
+			<div>
+				<div>{status}</div>
+				<button onClick={() => setStatus(status === 'online' ? 'offline' : 'online')}>Toggle Status</button>
+			</div>
 		</div>
 	)
 }
